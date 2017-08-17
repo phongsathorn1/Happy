c0481c62058cdd7c079920d90f66a64228b6d13e
Phongsathron <Phongsathron@outlook.com>
2017-08-17 23:17:32 +0700

    Update design

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,11 +1,15 @@
 @extends('layouts.auth')
 
 @section('content')
-<h1>Welcome To Happy Project</h1>
-<p>Sign in with your account to enjoy with Happy Project.</p>
-<div class="col-md-6 col-md-offset-3 login-panel">
-    <a href="{{ url('login/facebook') }}" class="btn btn-facebook-login"><i class="fa fa-facebook" aria-hidden="true"></i> Login with Facebook</a>
-    <a href="{{ url('login/google') }}" class="btn btn-google-login"><i class="fa fa-google" aria-hidden="true"></i> Login with Google</a>
-    <a href="{{ url('login/twitter') }}" class="btn btn-twitter-login"><i class="fa fa-twitter" aria-hidden="true"></i> Login with Twitter</a>
+<div class="clearfix">
+    <div class="col-md-6 login-description">
+        <h1>Welcome To Happy</h1>
+        <p>Sign in with your account to enjoy with Happy.</p>
+    </div>
+    <div class="col-md-6 login-panel">
+        <a href="{{ url('login/facebook') }}" class="btn btn-facebook-login"><i class="fa fa-facebook" aria-hidden="true"></i> Login with Facebook</a>
+        <a href="{{ url('login/google') }}" class="btn btn-google-login"><i class="fa fa-google" aria-hidden="true"></i> Login with Google</a>
+        <a href="{{ url('login/twitter') }}" class="btn btn-twitter-login"><i class="fa fa-twitter" aria-hidden="true"></i> Login with Twitter</a>
+    </div>
 </div>
 @endsection
