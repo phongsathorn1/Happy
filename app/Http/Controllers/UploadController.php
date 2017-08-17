c293fe26be5350ad4bc2617130b34d32eaea800c
Phongsathron <Phongsathron@outlook.com>
2017-08-17 23:18:53 +0700

    Update face detection and emotion

--- app/Http/Controllers/UploadController.php
+++ app/Http/Controllers/UploadController.php
@@ -75,11 +75,22 @@ class UploadController extends Controller
         $emotion_pass = true;
         $emotion_results = json_decode($data, true);
         $people_count = count($emotion_results);
+        list($img_width, $img_height, $img_type, $img_attr) = getimagesize($image_url);
+        $face_ractangles = [];
 
         if($people_count > 0){
             foreach($emotion_results as $value){
                 $result = array_keys($value["scores"], max($value["scores"]));
                 array_push($results, $result[0]);
+
+                //require image height
+                array_push($face_ractangles, [
+                    'top' => $value['faceRectangle']['top'] / $img_height * 100,
+                    'left' => $value['faceRectangle']['left']  / $img_width  * 100,
+                    'width' => $value['faceRectangle']['width'] / $img_width  * 100,
+                    'height' => $value['faceRectangle']['height'] / $img_height * 100,
+                    'emotion' => $result[0]
+                ]);
             }
 
             $emotion_pass = true;
@@ -101,7 +112,8 @@ class UploadController extends Controller
             'photo' => $filename,
             'emotion_pass' => $emotion_pass,
             'photo_emotion' => $photo_emotion,
-            'people_count' => $people_count
+            'people_count' => $people_count,
+            'face_rectangles' => $face_ractangles
         ]);
     }
 
@@ -115,6 +127,7 @@ class UploadController extends Controller
     {
         $this->validate($request, [
             'photo' => 'required',
+            'people_count' => 'required|integer',
             'description' => 'required'
         ]);
 
@@ -122,7 +135,7 @@ class UploadController extends Controller
         Post::create([
             'user_id' => Auth::id(),
             'photo' => $request->photo,
-            'people_count' => 1,
+            'people_count' => $request->people_count,
             'description' => $description,
         ]);
 
--- resources/views/create.blade.php
+++ resources/views/create.blade.php
@@ -9,12 +9,17 @@
     <div class="row">
         <div class="col-md-12">
             <div class="col-md-8 col-md-offset-2 img-post-view">
-                <img src="{{ url('storage/images/' . $folder .'/' . $photo . '.jpg') }}"><br>
+                <div class="img-block">
+                    @foreach($face_rectangles as $face_ractangle)
+                    <div class="faceborder {{ $face_ractangle['emotion'] }}" style="top: {{ $face_ractangle['top'] }}%; left: {{ $face_ractangle['left'] }}%; height: {{ $face_ractangle['height'] }}%; width: {{ $face_ractangle['width'] }}%"></div>
+                    @endforeach
+                    <img src="{{ url('storage/images/' . $folder .'/' . $photo . '.jpg') }}"><br>
+                </div>
                 <div class="photo-status">
                     <div class="photo-detail">
                         <span>{{ $people_count }} peoples in this photo.</span><br>
                     </div>
-                    <div class="emotion-badge">
+                    <div class="emotion-badge clearfix">
                         @foreach( $photo_emotion as $emotion => $count)
                             <div class="col-sx-1 emotion-badge-list emotion-{{ $emotion }}">
                                 <div class="emotion-icon-block">
@@ -28,8 +33,10 @@
                     </div>
                     @if($emotion_pass)
                         OK! I think you are happy 😁. Let post it!.
+                    @elseif($people_count == 0)
+                        We can't detect face in this picture 😅. <a href="{{ route('upload') }}" class="btn btn-info">Try Again</a>
                     @else
-                        Someone in this picture is not happy. 🙁
+                        Someone in this picture is not happy 🙁. <a href="{{ route('upload') }}" class="btn btn-info">Try Again</a>
                     @endif
                 </div>
                 @if($emotion_pass)
@@ -38,6 +45,7 @@
                         {{ csrf_field() }}
                         <b>description:</b>
                         <input type="hidden" name="photo" value="{{ $photo }}">
+                        <input type="hidden" name="people_count" value="{{ $people_count }}">
                         <textarea name="description"></textarea>
                         <input type="submit" class="btn btn-success" value="Just Post It!">
                     </form>
